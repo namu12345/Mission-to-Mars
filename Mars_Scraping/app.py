330c867f0212a2a46a3ef0c43484b1ca8cf9ff80
namu12345 <92283185+namu12345@users.noreply.github.com>
2021-12-29 12:31:12 -0600

    rename and folder created

--- Mars_Scraping/app.py
+++ Mars_Scraping/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scrapping
+import scraping
 
 app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
@@ -13,7 +13,7 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = scrapping.scrape_all()
+   mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 if __name__ == "__main__":
